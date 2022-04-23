2387c5ac0c21dd5a72549955a49fcd348283f59f
Andri <99926697+andrilucero@users.noreply.github.com>
2022-04-23 13:27:06 -0600

    Add files via upload

---
+++ rifas/Clases/ClsFnArregloRifa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace rifas.Clases
+{
+    internal class ClsFnArregloRifa
+    {
+        public int aleatorio(int n1, int n2)
+        {
+            {
+                var r = new Random();
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write("{0} ", r.Next(1, 11));
+                }
+
+                return r.Next(n1 -1, n2 -1);
+
+            }
+        }
+
+        public void Rifa(string[] nombresrifa, int premios)
+            {
+                for (int i = 0; i < premios; i++)
+                {
+                    int tombola = aleatorio(0, nombresrifa.Length);
+                    Console.Write($"Rifa numero {i} \t Tombola numero = {tombola}\n");
+
+                    Console.WriteLine($"\tEl ganador de esta rifa es {nombresrifa[tombola]}");
+                }
+
+            }
+        }
+    }
---
+++ rifas/Rifa.cs
@@ -0,0 +1,11 @@
+﻿using rifas.Clases;
+using c = System.Console;
+
+ClsFnArregloRifa arr = new ClsFnArregloRifa();
+
+string[] nombresrifa = { "Hector", "Sebastian", "Julissa", "Magaly", "Dylan", "Joel", "Andri", "Marielos", "Balvina", "Bryan", "Oscar" };
+
+
+arr.Rifa(nombresrifa, 9);
+
+
